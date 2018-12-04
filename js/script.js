77950228de45ca9a5fc58010c5caabf4143cde84
Mário Gago <mccpgago@gmail.com>
2018-12-04 22:41:06 +0000

    Folder structure

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var scroll = $('#phone-trigger').offset().top;
+var about = $('#about').offset().top;
+//console.log(scroll);
+
+$( window ).scroll(function() {
+  var view = $(window).scrollTop() + $(window).height();
+  //console.log(view);
+
+  if(view > scroll) {
+    $(".section-left-overlay").addClass("active");
+    $(".section-right").addClass("active");
+  }
+  if(view > about) {
+    $("#about").addClass("active");
+  }
+});
+
+function hide() {
+  $('#mce-error-response').css("display", "none");
+  $('#mce-success-response').css("display", "none");
+  $('#mce-error-response').html("");
+  $('#mce-success-response').html("");
+}
+function show() {
+  hide();
+  $('#mce-error-response').css("display", "block");
+  $('#mce-success-response').css("display", "block");
+}
+
+function openPrivacy() {
+  document.querySelector(".overlay-privacy").style.display = "flex";
+}
+function closePrivacy() {
+  document.querySelector(".overlay-privacy").style.display = "none";
+}
